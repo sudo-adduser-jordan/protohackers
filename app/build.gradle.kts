1a196f222527b8a25f7000304614ddc77e1431e1
jordan <sudo.sendmail.jordan@gmail.com>
2025-08-24 15:26:34 -0700

    update build / update setup

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,3 +41,7 @@ tasks.named<Test>("test") {
     // Use JUnit Platform for unit tests.
     useJUnitPlatform()
 }
+
+tasks.jar {
+    manifest.attributes["Main-Class"] = "server.Server"
+}
--- protohackers.sh
+++ protohackers.sh
@@ -1,17 +1,22 @@
-apt update
-apt upgrade
-apt install git
-apt install curl
-apt install gradle
+# deps
+apt update 
+apt upgrade 
+apt install -y git 
+apt install -y curl 
+apt install -y gradle 
+apt install -y zip
+apt install -y unzip
 
-apt install zip
-apt install unzip
+# java
 curl -s "https://get.sdkman.io" | bash
+source "$HOME/.sdkman/bin/sdkman-init.sh"
 sdk install java 24.0.2-graalce
 
-git clone https:github.com/sudo-adduser-jordan/protohackers
-gradle build protohackers
+# build
+git clone https:github.com/sudo-adduser-jordan/protohackers ~/protohackers
+gradle -p ~/protohackers build 
  
-java -jar server.jar &
-# pkill server.jar
+# run
+java -jar app/build/libs/app.jar 
+# pkill -f server.jar
 
