6c7bda4fac64effcf0556dc4df9c612a4bc0ad2c
jordan <sudo.sendmail.jordan@gmail.com>
2025-08-25 23:44:52 -0700

    Update RequestJSON.java

--- app/src/main/java/server/RequestJSON.java
+++ app/src/main/java/server/RequestJSON.java
@@ -1,9 +1,13 @@
 package server;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.Data;
 
 @Data // {"method":"isPrime","number":123} 
 public class RequestJSON {
+    @JsonProperty(required = true)
     private String method;    
+    @JsonProperty(required = true)
     private int number;
 }
