53d54e5ecb4cff167eaaa44ec6d0e5fe592e10c3
jordan <sudo.sendmail.jordan@gmail.com>
2025-08-26 01:27:29 -0700

    Update protohackers.sh

--- protohackers.sh
+++ protohackers.sh
@@ -8,6 +8,9 @@ apt install -y curl
 apt install -y zip
 apt install -y unzip
 
+# open port
+ufw allow 42069/tcp
+
 # java
 curl -s "https://get.sdkman.io" | bash
 source "$HOME/.sdkman/bin/sdkman-init.sh"
@@ -17,14 +20,10 @@ sdk install gradle 9.0.0
 # build
 git clone https://github.com/sudo-adduser-jordan/protohackers ~/protohackers
 cd ~/protohackers
-./gradlew build
+./gradlew run
 # git checkout 970df26b9dcfb0fbac74c266a5915fdbb3a24cb3 # solution 0
 # git checkout 970df26b9dcfb0fbac74c266a5915fdbb3a24cb3 # solution 1
 
-# open port
-ufw allow 42069/tcp
-
 # run
 # java -jar ~/protohackers/app/build/libs/app-all.jar &
-# pkill -f server.jar
 
