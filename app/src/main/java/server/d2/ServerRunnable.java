7e94f41bbfdb1d4535ab965fafb8b094da2fece5
jordan <sudo.sendmail.jordan@gmail.com>
2025-08-27 19:17:16 -0700

    Update ServerRunnable.java

--- app/src/main/java/server/d2/ServerRunnable.java
+++ app/src/main/java/server/d2/ServerRunnable.java
@@ -54,9 +54,7 @@ public class ServerRunnable implements Runnable
         {
             byte[] message = input.readNBytes(REQUEST_LENGTH);
             // if (message.length != REQUEST_LENGTH) {
-            if (message == null) throw new IllegalArgumentException("Message is null.");
-            
-            logger.debug(bytesToHex(message));
+            if (message.length == 0) throw new IllegalArgumentException("Message is empty.");
             if (message.length != REQUEST_LENGTH) throw new IllegalArgumentException("Message must be exactly 9 bytes");
 
             Request request = messageToRequest(message);
